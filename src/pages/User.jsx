0e0453a6a4fcf3d43eef3424b7ee31162466d687
Ray <rkilburg42@gmail.com>
2021-12-15 14:03:56 -0500

    get user repos

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -4,15 +4,17 @@ import {useParams} from 'react-router-dom'
 import {Link} from 'react-router-dom'
 import GithubContext from '../context/github/GithubContext'
 import Spinner from '../components/layout/Spinner'
+import RepoList from '../components/repos/RepoList'
 
 
 function User() {
-    const {getUser, user, loading} = useContext(GithubContext)
+    const {getUser, user, loading, getUserRepos, repos} = useContext(GithubContext)
 
     const params = useParams()
 
     useEffect(() => {
         getUser(params.login)
+        getUserRepos(params.login)
     }, [])
 
     const {
@@ -170,6 +172,8 @@ function User() {
                         </div>
                     </div>
                 </div>
+
+                <RepoList repos={repos} />
             </div>
         </>
     )
