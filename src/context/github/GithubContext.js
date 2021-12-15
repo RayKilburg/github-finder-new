aca86220ee5085fc40ff9eaf8148a8fb7d064a30
Ray <rkilburg42@gmail.com>
2021-12-14 21:20:13 -0500

    clear users

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -38,6 +38,9 @@ export const GithubProvider = ({children}) => {
         })
     }
 
+    // clear users from state
+    const clearUsers = () => dispatch({type:'CLEAR_USERS'})
+
     // set loading
     const setLoading = () => (dispatch({type: 'SET_LOADING'}))
 
@@ -45,6 +48,7 @@ export const GithubProvider = ({children}) => {
         users: state.users,
         loading: state.loading,
         searchUsers,
+        clearUsers
     }}>
         {children}
     </GithubContext.Provider>
