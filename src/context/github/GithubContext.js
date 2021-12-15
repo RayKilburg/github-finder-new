93795f829bad3658d64704c39a357077fa165775
Ray <rkilburg42@gmail.com>
2021-12-14 21:01:27 -0500

    search users

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -14,22 +14,27 @@ export const GithubProvider = ({children}) => {
 
     const [state, dispatch] = useReducer(githubReducer, intialState)
 
-    // get users from github api
-    const fetchUsers = async () => {
+    // get search results
+    const searchUsers = async (text) => {
         setLoading()
-        const response = await fetch(`${GITHUB_URL}/users`, {
+
+        const params = new URLSearchParams({
+            q: text
+        })
+
+        const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
             headers: {
                 Authorization: `token ${GITHUB_TOKEN}`
             }
         })
 
-        const data = await response.json()
+        const {items} = await response.json()
 
 
 
         dispatch({
             type: 'GET_USERS',
-            payload: data
+            payload: items
         })
     }
 
@@ -39,7 +44,7 @@ export const GithubProvider = ({children}) => {
     return <GithubContext.Provider value={{
         users: state.users,
         loading: state.loading,
-        fetchUsers,
+        searchUsers,
     }}>
         {children}
     </GithubContext.Provider>
